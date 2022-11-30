e0f6746dab05b6b85905783ec66b935f4a6eeebe
Thore <2120752@stud.hs-mannheim.de>
2022-11-30 17:04:01 +0100

    Assignment begonnen

--- assignement_05/site/script/Movie.php
+++ assignement_05/site/script/Movie.php
@@ -1,7 +1,13 @@
 <?php
-    $table = DOMDocument::getElementById("movie-table");
-    $table_body = DOMDocument::getElementById("tableBody");
-    $entries = [
-        ["Title" => "The Grudge", "Producer" => "Takashi Shimizu", "year" => "2005", "playtime" => "91", "fsk"=> "16"]
-    ];
+$table = DOMDocument::getElementById("movie-table");
+$table_body = DOMDocument::getElementById("tableBody");
+$entries = array(
+    array("Title" => "The Grudge", "Producer" => "Takashi Shimizu", "year" => "2005", "playtime" => "91", "fsk"=> "16"),
+    array("Title" => "The Grudge", "Producer" => "Takashi Shimizu", "year" => "2005", "playtime" => "91", "fsk"=> "16"),
+    array("Title" => "The Grudge", "Producer" => "Takashi Shimizu", "year" => "2005", "playtime" => "91", "fsk"=> "16"),
+    array("Title" => "The Grudge", "Producer" => "Takashi Shimizu", "year" => "2005", "playtime" => "91", "fsk"=> "16"),
+    array("Title" => "The Grudge", "Producer" => "Takashi Shimizu", "year" => "2005", "playtime" => "91", "fsk"=> "16"),
+    array("Title" => "The Grudge", "Producer" => "Takashi Shimizu", "year" => "2005", "playtime" => "91", "fsk"=> "16"),
+);
+print_r($entries)
 ?>
