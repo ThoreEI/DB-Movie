2bab55143294749ac44a03d4d853c4c85d49d7a2
Thore <2120752@stud.hs-mannheim.de>
2022-11-30 16:51:50 +0100

    Assignment begonnen

---
+++ assignement_05/site/script/Movie.php
@@ -0,0 +1,7 @@
+<?php
+    $table = DOMDocument::getElementById("movie-table");
+    $table_body = DOMDocument::getElementById("tableBody");
+    $entries = [
+        ["Title" => "The Grudge", "Producer" => "Takashi Shimizu", "year" => "2005", "playtime" => "91", "fsk"=> "16"]
+    ];
+?>
