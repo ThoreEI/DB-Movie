9b5d110354d64632ffc2b923db608b37c624cb4c
Marlon <xFatixHD@gmail.com>
2022-12-06 13:31:32 +0100

    zeigt alle Filme vom Array an

--- Assignment_06/site/index.php
+++ Assignment_06/site/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '\..\vendor\autoload.php';
+require_once "Film.php";
 
 use Twig\Environment;
 use Twig\TwigFilter;
@@ -7,5 +8,17 @@ use Twig\Loader\FilesystemLoader;
 
 $loader = new FilesystemLoader('.');
 $twig = new Environment($loader);
-$filme = array("123", "456", "789");
-echo $twig->render('template/index.html.twig', [ 'a_variable' => "asd" ]);
+
+$filme = array(
+    new \site\Film("The Grudge", "Takashi Shimizu", "2005", "91", "16"),
+    new \site\Film("Lucy", "Luc Besson", "2014", "89", "12"),
+    new \site\Film("Pulp Fiction", "Quentin Tarantino", "1994", "154", "16"),
+    new \site\Film("Inglorious Bastards", "Quentin Tarantino", "2009", "153", "16"),
+    new \site\Film("Reservoir Dogs", "Quentin Tarantino", "2005", "99", "18"),
+    new \site\Film("Blade Runner", "Ridley Scott", "1982", "117", "16"),
+);
+
+$twig->addGlobal('filme', $filme);
+echo $twig->render('template/index.html.twig', []);
+
+
