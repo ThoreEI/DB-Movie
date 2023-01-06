f3dd9044aa7893686538823973749cd806173563
Thore <2120752@stud.hs-mannheim.de>
2023-01-06 14:48:50 +0100

    Größe der Tabellenfelder (td) vereinheitlicht TODO bei delete button und Aufzählung nicht berücksichtigen

--- Assignment_07/site/index.php
+++ Assignment_07/site/index.php
@@ -11,11 +11,19 @@ use Twig\Extension\StringLoaderExtension;
 use Twig\Loader\FilesystemLoader;
 $twig = new Environment(new FilesystemLoader('../src/'));
 $twig->addExtension(new StringLoaderExtension());
+
 $data_base = new DbMovie();
 session_start();
-if (isset($_POST["add_movie"])) $data_base->insert_record($_POST["title"], $_POST["director"], $_POST["year"], $_POST["playtime"], $_POST["fsk"]);
-if (isset($_POST["delete_movie"])) $data_base->delete_movie($_POST["delete_movie"]);
-if (isset($_POST["reset_session"])) session_unset();
+
+if (isset($_POST["add_movie"]))
+    $data_base->insert_record($_POST["title"], $_POST["director"], $_POST["year"], $_POST["playtime"], $_POST["fsk"]);
+
+if (isset($_POST["delete_movie"]))
+    $data_base->delete_movie($_POST["delete_movie"]);
+
+if (isset($_POST["reset_session"]))
+    session_unset();
+
 $movies = $data_base->load_movies($_GET["sort_by"] ?? "title", $_POST["order"] ?? "asc");
 try { echo $twig->render('index.html.twig', ['movies' => $movies]);
 } catch (LoaderError|RuntimeError|SyntaxError $e) {
