7d2b19aa3fe3d5febc208f4477b61d4c81d0071f
Thore <2120752@stud.hs-mannheim.de>
2023-01-09 21:22:55 +0100

    sichtbarkeiten angepasst

--- Assignment_07/src/php/DbMovie.php
+++ Assignment_07/src/php/DbMovie.php
@@ -2,17 +2,17 @@
 namespace php;
 use PDO;
 class DbMovie {
-    private PDO $pdo;
+    private static PDO $pdo;
 
     public function __construct() {
-        $this->pdo = new PDO("sqlite:".join(DIRECTORY_SEPARATOR,[__DIR__,"..","database","movies.db"]));
-        $this->create_table();
-        if (empty($this->load_movies("title", "asc")))
+        self::$pdo = new PDO("sqlite:".join(DIRECTORY_SEPARATOR,[__DIR__,"..","database","movies.db"]));
+        self::create_table();
+        if (empty(self::load_movies("title", "asc")))
             $this->insert_default_movies();
     }
 
-    public function create_table() {
-       $this->pdo->prepare("CREATE TABLE IF NOT EXISTS t_movies (
+    private static function create_table() {
+      self::$pdo->prepare("CREATE TABLE IF NOT EXISTS t_movies (
                                     movieID INTEGER  PRIMARY KEY AUTOINCREMENT,
                                     title VARCHAR(150) NOT NULL, 
                                     director VARCHAR(150) NOT NULL,
@@ -21,13 +21,13 @@ class DbMovie {
                                     fsk INTEGER NOT NULL);")->execute();
     }
 
-    public function load_movies($sort_criterion, $order) {
+    public static function load_movies($sort_criterion, $order) {
         $sort_criterion = in_array($sort_criterion, ["director", "year", "playtime", "fsk"]) ? $sort_criterion : "title";
            $order = $order == "asc" ?  "ASC" : "DESC";
-        return $this->pdo->query("SELECT * FROM t_movies ORDER BY $sort_criterion $order")->fetchAll();
+        return self::$pdo->query("SELECT * FROM t_movies ORDER BY $sort_criterion $order")->fetchAll();
     }
 
-    public function insert_default_movies() {
+    public static function insert_default_movies() {
         $movies = [
             new Movie("The Grudge", "Takashi Shimizu", "2005", "91", "16"),
             new Movie("Lucy", "Luc Besson", "2014", "89", "12"),
@@ -37,18 +37,18 @@ class DbMovie {
             new Movie("Blade Runner", "Ridley Scott", "1982", "117", "16")
         ];
         foreach ($movies as $movie)
-            $this->insert_record($movie->title, $movie->director, $movie->year, $movie->playtime, $movie->fsk);
+            self::insert_record($movie->title, $movie->director, $movie->year, $movie->playtime, $movie->fsk);
     }
 
-    public function insert_record(string $title, string $director, int $year, int $playtime, int $fsk) {
-        $statement = $this->pdo->prepare("INSERT INTO t_movies (title, director, 'year', playtime, fsk)
+    public static function insert_record(string $title, string $director, int $year, int $playtime, int $fsk) {
+        $statement = self::$pdo->prepare("INSERT INTO t_movies (title, director, 'year', playtime, fsk)
                                                 VALUES (:title, :director, :year, :playtime, :fsk);");
         $statement->execute([":title"=>$title, ":director"=>$director, ":year"=>$year, ":playtime"=>$playtime, ":fsk"=>$fsk]);
         $statement=null;
     }
 
-    public function delete_movie(string $movieID) {
-        $statement = $this->pdo->prepare("DELETE FROM t_movies WHERE movieID = :movieID");
+    public static function delete_movie(string $movieID) {
+        $statement = self::$pdo->prepare("DELETE FROM t_movies WHERE movieID = :movieID");
         $statement->execute([":movieID"=>$movieID]);
     }
 }
