8f90527be1fef11cb701b63f4f9fcdcc746ce92e
Thore <mail@thoreeichhorn.de>
2023-01-25 19:22:13 +0100

    Default-movies angepasst

--- Assignment_07/src/php/DatabaseAbstractionLayer.php
+++ Assignment_07/src/php/DatabaseAbstractionLayer.php
@@ -13,13 +13,13 @@ class DatabaseAbstractionLayer {
     }
 
     public static function create_table(): void {
-        self::$pdo->prepare("CREATE TABLE IF NOT EXISTS t_movies (
+        self::$pdo->exec("CREATE TABLE IF NOT EXISTS t_movies (
                                     movieID INTEGER PRIMARY KEY AUTOINCREMENT,
                                     title VARCHAR(150) NOT NULL, 
                                     director VARCHAR(150) NOT NULL,
                                     'year' YEAR NOT NULL,
                                     playtime INTEGER NOT NULL,
-                                    fsk INTEGER NOT NULL);")->execute();
+                                    fsk INTEGER NOT NULL);");
     }
 
     public static function load_movies($sort_criterion = "title", $order="asc"): bool|array {
@@ -29,7 +29,14 @@ class DatabaseAbstractionLayer {
     }
 
     public static function insert_default_movies(): void {
-        $movies = [new Movie("The Grudge", "Takashi Shimizu", "2005", "91", "16"), new Movie("Lucy", "Luc Besson", "2014", "89", "12"), new Movie("Pulp Fiction", "Quentin Tarantino", "1994", "154", "16"), new Movie("Inglorious Bastards", "Quentin Tarantino", "2009", "153", "16"), new Movie("Reservoir Dogs", "Quentin Tarantino", "2005", "99", "18"), new Movie("Blade Runner", "Ridley Scott", "1982", "117", "16")];
+        $movies = [
+            new Movie("The Grudge", "Takashi Shimizu", "2005", "91", "16"),
+            new Movie("Lucy", "Luc Besson", "2014", "89", "12"),
+            new Movie("Pulp Fiction", "Quentin Tarantino", "1994", "154", "16"),
+            new Movie("Inglorious Bastards", "Quentin Tarantino", "2009", "153", "16"),
+            new Movie("Reservoir Dogs", "Quentin Tarantino", "2005", "99", "18"),
+            new Movie("Blade Runner", "Ridley Scott", "1982", "117", "16")
+        ];
         foreach ($movies as $movie)
             self::insert_record($movie->title, $movie->director, $movie->year, $movie->playtime, $movie->fsk);
     }
